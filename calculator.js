3bac46eb783495e282b875bdaaa4f6d025d2aa89
manal-coder <134100535+manal-coder@users.noreply.github.com>
2024-11-01 17:56:11 -0800

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,19 @@
+const calcBtn = document.querySelector('.calc-btn');
+
+
+let DisplayValue = '';
+
+function calcOperator(operator) {
+    DisplayValue += operator;
+    let display = document.querySelector('.input').value = DisplayValue
+}
+
+function calculate() {
+    let result = eval(DisplayValue)
+    let display = document.querySelector('.input').value = result;
+}
+
+function clearBtn() {
+    DisplayValue = '';
+    let display = document.querySelector('.input').value = '';
+}
